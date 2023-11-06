f18982244594266e01c27ef393d66cd8e16f1c55
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-11-05 22:36:21 -0600

    animations fixed

--- src/animations/CharacterAnimations.tsx
+++ src/animations/CharacterAnimations.tsx
@@ -11,7 +11,7 @@ const BerserkerBackground: React.FC = () => {
   const snowflakes = Array.from({ length: snowflakeCount }, (_, index) => {
     const style = {
       left: `${Math.random() * 100}%`,
-      animationDelay: `${-0.1 * (index + 1)}s`,
+      animationDelay: '0s',
       animationDuration: `${5 + Math.random() * 10}s`, 
     };
 
@@ -28,25 +28,92 @@ const BerserkerBackground: React.FC = () => {
 };
 
 const WarriorBackground: React.FC = () => {
-  const snowflakeCount = 30; 
-  const snowflakes = Array.from({ length: snowflakeCount }, (_, index) => {
+  const sparkCount = 40; 
+  const sparks = Array.from({ length: sparkCount }, (_, index) => {
     const style = {
-      left: `${Math.random() * 100}%`,
-      animationDelay: `${Math.random() * 5}s`,
-      animationDuration: `${5 + Math.random() * 10}s`, 
+      left: `${Math.random() * 95}%`, // Subtract the size of the spark to keep within bounds
+      animationDelay: '0s',
+      animationDuration: `${1 + Math.random() * 2}s`, 
     };
 
     return (
       <div
         key={index}
-        className="snowflake"
+        className="spark"
         style={style}
       />
     );
   });
 
-  return <>{snowflakes}</>;
+  return <>{sparks}</>;
 };
+
+const BardBackground: React.FC = () => {
+  const noteCount = 20; 
+  const notes = Array.from({ length: noteCount }, (_, index) => {
+    const style = {
+      left: `${Math.random() * 100}%`,
+      animationDelay: '0s',
+      animationDuration: `${2 + Math.random() * 3}s`, 
+      animationName: 'dance', 
+      animationTimingFunction: 'ease-in-out',
+      animationDirection: 'alternate',
+      animationIterationCount: 'infinite',
+      backgroundColor: `hsla(${Math.random() * 360}, 100%, 50%, 0.7)` 
+    };
+
+    return (
+      <div
+        key={index}
+        className="note orb"
+        style={style}
+      />
+    );
+  });
+
+  return <>{notes}</>;
+};
+
+const ClericBackground: React.FC = () => {
+  const orbCount = 20;
+  const orbs = Array.from({ length: orbCount }, (_, index) => {
+    const leftPosition = `calc(${Math.random() * 100}% - 20px)`;
+
+    const style = {
+      left: leftPosition,
+      animation: `moveUpAndAccelerate 5s ${Math.random() * 5}s infinite ease-in-out`
+    };
+
+    return <div key={index} className="healing-orb" style={style} />;
+  });
+
+  return <>{orbs}</>;
+};
+
+
+
+const WizardBackground: React.FC = () => {
+  const fireballCount = 20; 
+  const fireballs = Array.from({ length: fireballCount }, (_, index) => {
+    const style = {
+      top: `${10 + Math.random() * 80}%`, 
+      animationDelay: '0s', 
+      animationDuration: `${2 + Math.random() * 3}s`, 
+    };
+
+    return (
+      <div
+        key={index}
+        className="fireball"
+        style={style}
+      />
+    );
+  });
+
+  return <>{fireballs}</>;
+};
+
+
 
 
 const CharacterAnimations: React.FC<CharacterAnimationsProps> = ({ className }) => {
@@ -55,7 +122,12 @@ const CharacterAnimations: React.FC<CharacterAnimationsProps> = ({ className }) 
       return <BerserkerBackground />;
     case 'warrior':
       return <WarriorBackground />;
-    
+    case 'bard':
+      return <BardBackground />;
+    case 'cleric':
+      return <ClericBackground />;
+    case 'wizard':
+      return <WizardBackground />;
     default:
       return null; 
   }
